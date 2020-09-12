2884402d4850ab479cfc659685d0fe776cb9a913
GLino-Oficial <gabriellinodovalle@gmail.com>
2020-09-12 17:15:59 -0300

    Sobe

---
+++ POO3B1_14E21/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace POO3B1_14E21
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ POO3B1_14E21/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="POO3B1_14E21.MvcApplication" Language="C#" %>
